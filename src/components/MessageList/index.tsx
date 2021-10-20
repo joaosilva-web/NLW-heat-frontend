d5871ef8806574e93ebfc742dff1ebb541a9c630
João Gustavo <joaogustavosilva2001@gmail.com>
2021-10-20 15:33:02 -0300

    create service/api.ts and consume it in MessageList component

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,24 +1,49 @@
+import { api } from "../../services/api";
+
 import S from './styles.module.scss';
 
 import logo from '../../assets/logo.svg';
+import { useEffect, useState } from "react";
+
+type Message = {
+    id: string;
+    text: string;
+    user: {
+        name: string;
+        avatar_url: string;
+    }
+}
 
 export function MessageList() {
+    const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        api.get<Message[]>('messages/last3').then(res => {
+            setMessages(res.data);
+        })
+    }, [])
+
     return(
         <div className={S.messageListWrapper}>
             <img src={logo} alt="DoWhile 2021" />
 
             <ul className={S.messageList}>
-                <li className={S.message}>
-                    <p className={S.messageContent}>
-                    Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥
-                    </p>
-                    <div className={S.messageUser}>
-                        <div className={S.userImage}>
-                            <img src="https://github.com/joaosilva-web.png" alt="João Gustavo" />
-                        </div>
-                        <span>João Gustavo</span>
-                    </div>
-                </li>
+
+                {messages.map(message => {
+                    return (     
+                        <li key={message.id} className={S.message}>
+                            <p className={S.messageContent}>
+                            {message.text}
+                            </p>
+                            <div className={S.messageUser}>
+                                <div className={S.userImage}>
+                                    <img src={message.user.avatar_url} alt={message.user.name} />
+                                </div>
+                                <span>{message.user.name}</span>
+                            </div>
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
