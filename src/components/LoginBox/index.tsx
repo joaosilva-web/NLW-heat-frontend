be4c20e824b8ddec0ab06c45ebb22f65eba19b5f
João Gustavo <joaogustavosilva2001@gmail.com>
2021-10-20 16:26:40 -0300

    create authContext

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,12 +1,17 @@
+import { useContext } from "react";
 import { VscGithubInverted } from "react-icons/vsc";
+import { AuthContext } from "../../contexts/authContext";
 
 import S from "./styles.module.scss";
 
 export function LoginBox() {
+    const { signInUrl } = useContext(AuthContext);
+
+
     return(
         <div className={S.loginBoxWrapper}>
             <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={S.signInWithGithub}>
+            <a href={signInUrl} className={S.signInWithGithub}>
                 <VscGithubInverted size="24"/>
                 entrar com github
             </a>
